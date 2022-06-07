5cf01a88a6aff8fc1394088ffed93158e12bdae6
Elle Jacobs <enj657@gmail.com>
2022-06-07 13:11:42 -0500

    Adding flexbox css styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ export default function Card(props) {
     return (
         <div className="card">
             <div className="card-img-container">
-                <img className="card-img" src={require(`../images/${props.item.img}`)} alt="here"/>
+                <img className="card-img" src={require(`../images/${props.item.img}`)} alt="here" />
             </div>
             <div className="card-info">
                 <div className="card-location-info">
@@ -12,9 +12,11 @@ export default function Card(props) {
                     <span className="location">{props.item.location}</span>
                     <a className="maps-link" href={props.item.googleMapsUrl}>View on Google Maps</a>
                 </div>
-                <span className="card-title">{props.item.title}</span>
-                <span className="card-date">{props.item.startDate} - {props.item.endDate}</span>
-                <p className="card-description">{props.item.description}</p>
+                <div className="card-body">
+                    <span className="card-title">{props.item.title}</span>
+                    <span className="card-date">{props.item.startDate} - {props.item.endDate}</span>
+                    <p className="card-description">{props.item.description}</p>
+                </div>
             </div>
         </div>
     )
